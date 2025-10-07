ff34508b180217cf5953dd11f9d2292608affcf3
diegotosato <tosato1998@gmail.com>
2025-10-07 16:32:11 +0200

    copy/paste exercise track; declare tools; declare task to solve the exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
-console.log('It Works');
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+
+
+
+// Tools:
+// Loop --> for (counter; condition; increase;)
+// arithmetics operator --> === equal value and equal type; % modulus;
+// conditional instruction --> if/else
+
+
+
+
+//Task
+
+//#1 - Scrivi un programma che stampi i numeri da 1 a 100
+    // "for" syntax with "i" initial value 1 with <= 100 condition
+
+//#2 - per i multipli di 3 stampi “Fizz” al posto del numero
+    // new variable --> fizz
+    // i % 3 === 0 --> Fizz
+
+//#3 - per i multipli di 5 stampi Buzz
+    // new variable --> buzz
+    // i % 5 === 0 --> Buzz
+
+//#4 - Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+    // i % (3 && 5) === 0 --> FizzBuzz --> ${fizz}${buzz}
