2056cb0d1dc7cae2d13c42528f5e09ecd9a1368a
diegotosato <tosato1998@gmail.com>
2025-10-07 16:44:24 +0200

    add second IF condition
    add second if condition when "i" is divisible for 5; if is true log the string Buzz; if is false contuinue to show numbers, check if it works

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,17 +25,19 @@
 //Task
 
 //#1 - Scrivi un programma che stampi i numeri da 1 a 100
-    // "for" syntax with "i" initial value 1 with <= 100 condition
+// "for" syntax with "i" initial value 1 with <= 100 condition
 for (let i = 1; i <= 100; i++) {
-    // console.log(i);
 
     //#2 - per i multipli di 3 stampi “Fizz” al posto del numero
-        // i % 3 === 0 --> Fizz
-    if (i % 3 === 0){
+    // i % 3 === 0 --> Fizz
+    if (i % 3 === 0) {
         console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+        //#3 - per i multipli di 5 stampi Buzz
+        // i % 5 === 0 --> Buzz
     } else {
         console.log(i);
-        
     }
 
 
@@ -44,8 +46,6 @@ for (let i = 1; i <= 100; i++) {
 }
 
 
-//#3 - per i multipli di 5 stampi Buzz
-    // i % 5 === 0 --> Buzz
 
 //#4 - Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
-    // i % (3 && 5) === 0 --> FizzBuzz --> ${fizz}${buzz}
+// i % (3 && 5) === 0 --> FizzBuzz --> ${fizz}${buzz}
